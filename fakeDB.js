79611cdbe01041edaafd3687d15519d3bda33753
Dattn <dattn104@gmail.com>
2024-05-02 09:41:11 +0700

    Add files via upload

---
+++ fakeDB.js
@@ -0,0 +1,104 @@
+const resultFake = {
+  "result_code": 0,
+  "data": {
+    "event_end_date": "30/04/2024",
+    "award": [
+      {
+        "award_id": "award_af-5b47-4147-a95a-46bb3b8ed50d",
+        "total_piece": "7",
+        "award_name": "Ba",
+        "award_image": "https://cdn-devtest.acbs.tech/assets/image/quaysotrungthuong2024/z5322095083909_a7f5b11c49e17b05753ef455f5380cca.jpg"
+      },
+      {
+        "award_id": "award_38-8bc1-48ff-9986-f7832c6431c2",
+        "total_piece": "24",
+        "award_name": "Đặc biệt",
+        "award_image": "https://cdn-devtest.acbs.tech/assets/image/quaysotrungthuong2024/z5322095083909_a7f5b11c49e17b05753ef455f5380cca.jpg"
+      },
+      {
+        "award_id": "award_8f-d35c-4533-88c6-91abd322cc93",
+        "total_piece": "11",
+        "award_name": "Nhì",
+        "award_image": "https://cdn-devtest.acbs.tech/assets/image/quaysotrungthuong2024/z5322095083909_a7f5b11c49e17b05753ef455f5380cca.jpg"
+      },
+      {
+        "award_id": "award_32-31b4-45bf-8957-a9b7fab35a2d",
+        "total_piece": "11",
+        "award_name": "Nhất",
+        "award_image": "https://cdn-devtest.acbs.tech/assets/image/quaysotrungthuong2024/z5322095083909_a7f5b11c49e17b05753ef455f5380cca.jpg"
+      }
+    ],
+    "piece": [
+      {
+        "award_id": "award_38-8bc1-48ff-9986-f7832c6431c2",
+        "piece_id": "DB1",
+        "name": "db normal",
+        "key": "false",
+        "image": "https://cdn-devtest.acbs.tech/assets/image/quaysotrungthuong2024/z5322095083909_a7f5b11c49e17b05753ef455f5380cca.jpg"
+      },
+      {
+        "award_id": "award_32-31b4-45bf-8957-a9b7fab35a2d",
+        "piece_id": "Nhata",
+        "name": "normal 1",
+        "key": "false",
+        "image": "https://cdn-devtest.acbs.tech/assets/image/quaysotrungthuong2024/z5322095000776_257eca051aee0703c6cf6a0e0af90dd8.jpg"
+      },
+      {
+        "award_id": "award_32-31b4-45bf-8957-a9b7fab35a2d",
+        "piece_id": "Nhatb",
+        "name": "normal 1",
+        "key": "false",
+        "image": "https://cdn-devtest.acbs.tech/assets/image/quaysotrungthuong2024/z5322095000776_257eca051aee0703c6cf6a0e0af90dd8.jpg"
+      },
+      {
+        "award_id": "award_af-5b47-4147-a95a-46bb3b8ed50d",
+        "piece_id": "Ba2",
+        "name": "normal 5",
+        "key": "false",
+        "image": "https://cdn-devtest.acbs.tech/assets/image/quaysotrungthuong2024/z5322095000776_257eca051aee0703c6cf6a0e0af90dd8.jpg"
+      },
+      {
+        "award_id": "award_38-8bc1-48ff-9986-f7832c6431c2",
+        "piece_id": "DB2",
+        "name": "db normal",
+        "key": "false",
+        "image": "https://cdn-devtest.acbs.tech/assets/image/quaysotrungthuong2024/z5322095083909_a7f5b11c49e17b05753ef455f5380cca.jpg"
+      },
+      {
+        "award_id": "award_af-5b47-4147-a95a-46bb3b8ed50d",
+        "piece_id": "Ba1",
+        "name": "normal 5",
+        "key": "false",
+        "image": "https://cdn-devtest.acbs.tech/assets/image/quaysotrungthuong2024/z5322095000776_257eca051aee0703c6cf6a0e0af90dd8.jpg"
+      },
+      {
+        "award_id": "award_8f-d35c-4533-88c6-91abd322cc93",
+        "piece_id": "Nhia",
+        "name": "normal 2",
+        "key": "false",
+        "image": "https://cdn-devtest.acbs.tech/assets/image/quaysotrungthuong2024/z5322095083909_a7f5b11c49e17b05753ef455f5380cca.jpg"
+      },
+      {
+        "award_id": "award_38-8bc1-48ff-9986-f7832c6431c2",
+        "piece_id": "DB2",
+        "name": "db normal",
+        "key": "false",
+        "image": "https://cdn-devtest.acbs.tech/assets/image/quaysotrungthuong2024/z5322095083909_a7f5b11c49e17b05753ef455f5380cca.jpg"
+      },
+      {
+        "award_id": "award_38-8bc1-48ff-9986-f7832c6431c2",
+        "piece_id": "DB1",
+        "name": "db normal",
+        "key": "false",
+        "image": "https://cdn-devtest.acbs.tech/assets/image/quaysotrungthuong2024/z5322095083909_a7f5b11c49e17b05753ef455f5380cca.jpg"
+      },
+      {
+        "award_id": "award_32-31b4-45bf-8957-a9b7fab35a2d",
+        "piece_id": "Nhata",
+        "name": "normal 1",
+        "key": "false",
+        "image": "https://cdn-devtest.acbs.tech/assets/image/quaysotrungthuong2024/z5322095083909_a7f5b11c49e17b05753ef455f5380cca.jpg"
+      }
+    ]
+  }
+}
