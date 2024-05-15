5db154b3796f0b5c53098027aa6e9b3fa9235b19
DATTN <112852034+datnt-acbs-com-vn@users.noreply.github.com>
2024-05-15 09:58:33 +0700

    update picture

--- fakeDB.js
+++ fakeDB.js
@@ -102,3 +102,115 @@ const resultFake = {
     ]
   }
 }
+
+const inventoryFake = {
+  "result_code": 0,
+  "data": {
+    "awards": [
+      {
+        "award_id": "award_47-c7fa-425a-a98e-beb175117eb0",
+        "award_name": "Giải 4",
+        "award_info": "Vali Traveler's choice",
+        "award_image": "https://cdn-devtest.acbs.tech/assets/image/quaysotrungthuong2024/giai_thuong/giai-tu.png",
+        "pieces": [
+          {
+            "piece_id": "award_piece_c-416b-bd8b-4b7035cae1b9",
+            "piece_name": "Mảnh dưới phải giải 4",
+            "piece_amount": "1",
+            "piece_key": "False",
+            "piece_image": "https://cdn-devtest.acbs.tech/assets/image/quaysotrungthuong2024/giai_tu/manh-ghep-giai-tu-20.png",
+            "piece_position": "BottomRight"
+          },
+          {
+            "piece_id": "award_piece_8-4625-871b-da24abb318f7",
+            "piece_name": "Mảnh dưới trái giải 4",
+            "piece_amount": "1",
+            "piece_key": "False",
+            "piece_image": "https://cdn-devtest.acbs.tech/assets/image/quaysotrungthuong2024/giai_tu/manh-ghep-giai-tu-19.png",
+            "piece_position": "BottomLeft"
+          }
+        ]
+      },
+      {
+        "award_id": "award_c4-45b6-4548-9b9a-2b79616715eb",
+        "award_name": "Giải nhất",
+        "award_info": "iPad Pro m2 12.9 inch 256GB",
+        "award_image": "https://cdn-devtest.acbs.tech/assets/image/quaysotrungthuong2024/giai_thuong/giai-nhat.png",
+        "pieces": [
+          {
+            "piece_id": "award_piece_9-4148-9c98-763ee5808e28",
+            "piece_name": "Mảnh trên trái giải nhất",
+            "piece_amount": "1",
+            "piece_key": "False",
+            "piece_image": "https://cdn-devtest.acbs.tech/assets/image/quaysotrungthuong2024/giai_nhat/manh-ghep-giai-nhat-05.png",
+            "piece_position": "TopLeft"
+          },
+          {
+            "piece_id": "award_piece_6-40ad-814b-fdbe3567b7b0",
+            "piece_name": "Mảnh dưới trái giải nhất",
+            "piece_amount": "1",
+            "piece_key": "False",
+            "piece_image": "https://cdn-devtest.acbs.tech/assets/image/quaysotrungthuong2024/giai_nhat/manh-ghep-giai-nhat-07.png",
+            "piece_position": "BottomLeft"
+          }
+        ]
+      },
+      {
+        "award_id": "award_1c-db4f-4a47-9b8b-a1c5acd06048",
+        "award_name": "Giải đặc biệt",
+        "award_info": "MacBook Pro 14 inch M3",
+        "award_image": "https://cdn-devtest.acbs.tech/assets/image/quaysotrungthuong2024/giai_thuong/giai-dac-biet.png",
+        "pieces": [
+          {
+            "piece_id": "award_piece_1-4aa7-a4b2-79f0721f7dc9",
+            "piece_name": "Mảnh trên phải đặc biệt",
+            "piece_amount": "2",
+            "piece_key": "False",
+            "piece_image": "https://cdn-devtest.acbs.tech/assets/image/quaysotrungthuong2024/giai_dac_biet/manh-ghep-giai-db-02.png",
+            "piece_position": "TopRight"
+          },
+          {
+            "piece_id": "award_piece_9-4ca2-8780-d3498b1827eb",
+            "piece_name": "Mảnh dưới trái đặc biệt",
+            "piece_amount": "1",
+            "piece_key": "False",
+            "piece_image": "https://cdn-devtest.acbs.tech/assets/image/quaysotrungthuong2024/giai_dac_biet/manh-ghep-giai-db-03.png",
+            "piece_position": "BottomLeft"
+          }
+        ]
+      },
+      {
+        "award_id": "award_a3-4d05-4ad2-906a-e82b634fa359",
+        "award_name": "Giải nhì",
+        "award_info": "iPhone 15 Pro Max 256GB",
+        "award_image": "https://cdn-devtest.acbs.tech/assets/image/quaysotrungthuong2024/giai_thuong/giai-nhi.png",
+        "pieces": [
+          {
+            "piece_id": "award_piece_8-4519-8c90-34bcb025ba81",
+            "piece_name": "Mảnh dưới phải giải nhì",
+            "piece_amount": "1",
+            "piece_key": "False",
+            "piece_image": "https://cdn-devtest.acbs.tech/assets/image/quaysotrungthuong2024/giai_nhi/manh-ghep-giai-nhi-12.png",
+            "piece_position": "BottomRight"
+          },
+          {
+            "piece_id": "award_piece_3-4450-93ec-41e4cd18f9c2",
+            "piece_name": "Mảnh trên trái giải nhì",
+            "piece_amount": "1",
+            "piece_key": "False",
+            "piece_image": "https://cdn-devtest.acbs.tech/assets/image/quaysotrungthuong2024/giai_nhi/manh-ghep-giai-nhi-09.png",
+            "piece_position": "TopLeft"
+          },
+          {
+            "piece_id": "award_piece_b-4600-883e-7c4b72648488",
+            "piece_name": "Mảnh trên phải giải nhì",
+            "piece_amount": "1",
+            "piece_key": "False",
+            "piece_image": "https://cdn-devtest.acbs.tech/assets/image/quaysotrungthuong2024/giai_nhi/manh-ghep-giai-nhi-10.png",
+            "piece_position": "TopRight"
+          }
+        ]
+      }
+    ]
+  }
+}
